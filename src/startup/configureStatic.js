34903cc0d188a4d9c02e293925d73757d5903a7e
Andrew Ramsay <aramsay@fusionalliance.com>
2016-02-02 18:06:35 -0500

    Added ability for static routes to specify a url

--- src/startup/configureStatic.js
+++ src/startup/configureStatic.js
@@ -4,8 +4,16 @@ const _ = require('lodash');
 const express = require('express');
 
 function configureStatic(simpleServer) {
-  _.forEach(simpleServer._statics, function (path) {
-    simpleServer._app.use(express.static(path));
+  _.forEach(simpleServer._statics, function (staticRoute) {
+    if (typeof staticRoute === 'string') {
+      staticRoute = { path: staticRoute };
+    }
+
+    if (staticRoute.url) {
+      simpleServer._app.use(staticRoute.url, express.static(staticRoute.path));
+    } else {
+      simpleServer._app.use(express.static(staticRoute.path));
+    }
   });
 }
 
