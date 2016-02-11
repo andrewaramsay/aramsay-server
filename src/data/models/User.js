6ff694b22ab02a1172efa62a315043e0c3e6682d
Andrew Ramsay <aramsay@fusionalliance.com>
2016-02-10 23:47:42 -0500

    Added display name to user model

--- src/data/models/User.js
+++ src/data/models/User.js
@@ -9,6 +9,7 @@ const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
   username: { type: String, required: true },
+  displayName: { type: String, required: true },
   password: { type: String, required: true },
   loginAttempts: { type: Number, default: 0 },
   roles: [{ type: String, ref: 'Role' }]
