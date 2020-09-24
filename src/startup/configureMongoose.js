f68a1f0b2578a3b60d1fd49c771abd639826f1cb
Andrew Ramsay <aramsaybusiness@gmail.com>
2020-09-23 20:15:55 -0400

    Addressing warnings in new version of mongoose

--- src/startup/configureMongoose.js
+++ src/startup/configureMongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 function configureMongoose(options) {
   mongoose.Promise = Promise;
-  mongoose.connect(options.mongoConnectionString());
+  mongoose.connect(options.mongoConnectionString(), { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 module.exports = configureMongoose;
